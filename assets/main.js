911a297c1688bf2c49aa96137df27ffec5431d31
Victoria Gonzalez <vgonzalez3092@gmail.com>
2020-09-22 20:30:04 -0400

    site redesign

--- assets/main.js
+++ assets/main.js
@@ -1,144 +1,180 @@
-//OPEN TRIGGER
-var openTrigger = $('.menu-trigger');
-var openTriggerTop = openTrigger.find('.menu-trigger-bar.top');
-var openTriggerMiddle = openTrigger.find('.menu-trigger-bar.middle');
-var openTriggerBottom = openTrigger.find('.menu-trigger-bar.bottom');
-
-//CLOSE TRIGGER
-var closeTrigger = $('.close-trigger');
-var closeTriggerLeft = closeTrigger.find('.close-trigger-bar.left');
-var closeTriggerRight = closeTrigger.find('.close-trigger-bar.right');
-
-//LOGO
-var logo = $('.logo');
-
-//MENU
-var menuContainer = $('.menu-container');
-var menu = $('.menu');
-var menuTop = $('.menu-bg.top');
-var menuMiddle = $('.menu-bg.middle');
-var menuBottom = $('.menu-bg.bottom');
-
-//TL
-var tlOpen = new TimelineMax({paused: true});
-var tlClose = new TimelineMax({paused: true});
-
-//OPEN TIMELINE
-tlOpen.add("preOpen")
-  .to(logo, 0.4, {
-            scale: 0.8,
-            opacity: 0,
-            ease: Power2.easeOut
-        }, "preOpen")
-.to(openTriggerTop, 0.4, {
-  x: "+80px", y: "-80px", delay: 0.1, ease: Power4.easeIn, onComplete: function() {
-    closeTrigger.css('z-index','25');
+'use strict';
+// Abandon all hope ye who enter here
+$(document).ready(function() {
+  
+  var navigating = false,
+      curPage = 1,
+      pages = $(".section").length,
+      $sections = $(".sections"),
+      $paginationPage = $(".pagination .page"),
+      $paginationTotal = $(".total-pages"),
+      $textStuff = $(".section-heading, .additional-text");
+  
+  if (pages >= 10) {
+    $paginationTotal.text(pages);
+  } else {
+    $paginationTotal.text("0" + pages);
   }
-}, "preOpen")
-.to(openTriggerMiddle, 0.4, {
-  x: "+=80px", y: "-=80px", ease: Power4.easeIn,
-  onComplete: function() {
-    openTrigger.css('visibility','hidden');
+  
+  /*
+  Sets random transition-delay for blocks between 0.4 and 1.2 seconds on every call
+  */
+  function randomDelay() {
+    $(".left-part").css("transition-delay", (Math.floor(Math.random() * 9) + 4)/10 + "s");
+    for (var i = 1; i <= pages; i++) {
+      $(".bg-part:nth-child("+i+")").css("transition-delay", (Math.floor(Math.random() * 9) + 4)/10 + "s");
+    }
   }
-}, "preOpen")
-.to(openTriggerBottom, 0.4, {
-  x: "+=80px", y: "-=80px", delay: 0.2, ease: Power4.easeIn
-}, "preOpen")
-.add("open", "-=0.4")
-.to(menuTop, 0.8, {
-  y: "13%",
-  ease: Power4.easeInOut
-}, "open")
-.to(menuMiddle, 0.8, {
-  scaleY: 5.5,
-  ease: Power4.easeInOut
-}, "open")
-.to(menuBottom, 0.8, {
-  y: "-114%",
-  ease: Power4.easeInOut
-}, "open")
-.fromTo(menu, 0.6, {
-  y: 30, opacity: 0, visibility: 'hidden'
-}, {
-  y: 0, opacity: 1, visibility: 'visible', ease: Power4.easeOut
-}, "-=0.2")
-.add("preClose", "-=0.8")
-.to(closeTriggerLeft, 0.8, {
-  x: "-=100px", y: "+=100px", ease: Power4.easeOut
-}, "preClose")
-.to(closeTriggerRight, 0.8, {
-  x: "+=100px", y: "+=100px", delay: 0.2, ease: Power4.easeOut
-}, "preClose");
-
-//CLOSE TIMELINE
-tlClose.add("close")
-  .to(menuTop, 0.2, {
-  backgroundColor: "#5c5b5b", ease: Power4.easeInOut, onComplete: function() {
-    logo.css('z-index','26');
-    closeTrigger.css('z-index','5');
- openTrigger.css('visibility','visible');
+  
+  /* Async hell, with hardcoded numbers.
+  On production, 410 number must be .section-heading transform transition time in miliseconds + 10, but i'm sort of tired of this demo :D
+  */
+  function timeoutNav(t) {
+    var time = t || 2000;
+    $textStuff.addClass("not-visible");
+    setTimeout(function() {
+      navigating = false;
+      randomDelay();
+    }, time);
+    setTimeout(function() {
+      // cached selector not working because of newely created clone when moving up more then 2 positions
+      $(".section-heading, .additional-text").css({"margin-top": 0 - (parseInt($(".nav-elem.active").attr("data-page")) - 1) * 100 + "vh"}).hide();
+    }, 410);
+    setTimeout(function() {
+      $textStuff.show();
+      $textStuff.css("top");
+      $textStuff.removeClass("not-visible");
+    }, time + 10);
   }
-}, "close")
-.to(menuMiddle, 0.2, {
-  backgroundColor: "#5c5b5b", ease: Power4.easeInOut
-}, "close") 
-.to(menuBottom, 0.2, {
-  backgroundColor: "#5c5b5b", ease: Power4.easeInOut
-}, "close")
-  .to(menu, 0.6, {
-  y: 20, opacity: 0, ease: Power4.easeOut, onComplete: function() {
-    menu.css('visibility','hidden');
+  
+  function magicStuff(paramPage) {
+    if (paramPage) curPage = paramPage;
+    navigating = true;
+    var calculatedMargin = 0 - (curPage - 1) * 100;
+    $(".bg-part, .left-part").css("margin-top", calculatedMargin +"vh");
+    $(".scroll-down").addClass("removed");
+    if (parseInt($(".nav-elem.active").attr("data-page")) === 1) {
+      $(".scroll-down").removeClass("removed");
+    }
   }
-}, "close")
-.to(logo, 0.8, {
-  scale: 1, opacity: 1, ease: Power4.easeInOut
-}, "close", "+=0.2")
-.to(menuTop, 0.8, {
-  y: "-113%",
-  ease: Power4.easeInOut
-}, "close", "+=0.2")
-.to(menuMiddle, 0.8, {
-  scaleY: 0,
-  ease: Power4.easeInOut
-}, "close", "+=0.2")
-.to(menuBottom, 0.8, {
-  y: "23%",
-  ease: Power4.easeInOut,
-  onComplete: function() {
-    menuTop.css('background-color','#ffffff');
-    menuMiddle.css('background-color','#ffffff');
-    menuBottom.css('background-color','#ffffff');
+  
+  function trickyStuff(page) {
+    $(".left-part, .bg-part").css({"transition-duration": "0s", "transition-delay": "0s"});
+    $(".main").css("top");
+    magicStuff(page);
+    $(".main").css("top");
+    $(".left-part, .bg-part").css("transition-duration", "0.8s");
+    randomDelay();
   }
-}, "close", "+=0.2")
-.to(closeTriggerLeft, 0.2, {
-  x: "+=100px", y: "-=100px", ease: Power4.easeIn
-}, "close")
-.to(closeTriggerRight, 0.2, {
-  x: "-=100px", y: "-=100px", delay: 0.1, ease: Power4.easeIn
-}, "close")
-.to(openTriggerTop, 1, {
-  x: "-=80px", y: "+=80px", delay: 0.2, ease: Power4.easeOut
-}, "close")
-.to(openTriggerMiddle, 1, {
-  x: "-=80px", y: "+=80px", ease: Power4.easeOut
-}, "close")
-.to(openTriggerBottom, 1, {
-  x: "-=80px", y: "+=80px", delay: 0.1, ease: Power4.easeOut
-}, "close");
-
-//EVENTS
-openTrigger.on('click', function(){
-  if(tlOpen.progress() < 1){
-                tlOpen.play();
-            } else {
-                tlOpen.restart();
-            }
-});
-       
-closeTrigger.on('click', function(){
-  if(tlClose.progress() < 1){
-                tlClose.play();
-            } else {
-                tlClose.restart();
-            }
+  
+  function pagination(pg) {
+    $(".nav-elem").removeClass("active");
+    $(".nav-" + pg).addClass("active");
+    curPage = pg;
+    
+    if (pages >= 10) {
+      $paginationPage.text(pg);
+    } else {
+      $paginationPage.text("0" + pg);
+    }
+  }
+  
+  function navigateUp() {
+    if (curPage > 1) {
+      curPage--;
+      pagination(curPage);
+      magicStuff();
+      timeoutNav();
+    }
+  }
+  
+  function navigateDown() {
+    if (curPage < pages) {
+      curPage++;
+      pagination(curPage);
+      magicStuff();
+      timeoutNav();
+    }
+  }
+  
+  $(document).on("mousewheel DOMMouseScroll", function(e) {
+    if (!navigating) {
+      if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+        navigateUp();
+      } else { 
+        navigateDown();
+      }
+    }
+  });
+  
+  $(document).on("mousewheel DOMMouseScroll",
+                 ".sidebar-hover, .sidebar-real",
+                 function(e) {
+    e.stopPropagation();
+  });
+  
+  var sidebarScroll = 0,
+      $navEl =  $(".nav-elem"),
+      $sidebar = $(".sidebar-real"),
+      maxScroll = $navEl.length * $navEl.height() - $(window).height();
+  
+  $(document).on("mousewheel DOMMouseScroll", ".sidebar-real", function(e) {
+    if (navigating) return;
+    if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+      if (!sidebarScroll) return;
+      sidebarScroll += 100;
+      if (sidebarScroll > 0) sidebarScroll = 0;
+    } else { 
+      if (Math.abs(sidebarScroll) === maxScroll) return;
+      sidebarScroll -= 100;
+      if (Math.abs(sidebarScroll) > maxScroll) sidebarScroll = 0 - maxScroll;
+    }
+    $sidebar.css("transform", "translateY("+ sidebarScroll +"px)");
+  });
+  
+  $(document).on("click", ".nav-elem:not(.active)", function() {
+    if (navigating) return;
+    var activePage = parseInt($(".nav-elem.active").attr("data-page"), 10),
+        futurePage = $(this).attr("data-page");
+    
+    pagination(futurePage);
+    
+    if (Math.abs(activePage - futurePage) > 2) {
+      var $fakePage = $(".section-" + futurePage).clone(),
+          $currentPage = $(".section-" + activePage),
+          fakeNumber = 0;
+      // ugly code, do not enter here
+      if (activePage < futurePage) {
+        // moving down
+        $currentPage.after($fakePage);
+        fakeNumber = activePage + 1;
+        $(".main").css("top");
+        randomDelay();
+        magicStuff(fakeNumber);
+      } else {
+        // moving up (real hell)
+        $currentPage.before($fakePage);
+        fakeNumber = activePage - 1;
+        trickyStuff(activePage + 1);
+        $(".main").css("top");
+        randomDelay();
+        $(".main").css("top");
+        magicStuff(activePage);
+      }
+      timeoutNav(2050);
+      setTimeout(function() {
+        $fakePage.remove();
+        trickyStuff(futurePage);
+      }, 2000);
+    } else {
+      magicStuff(futurePage);
+      timeoutNav();
+    }
+  });
+  
+  $(window).resize(function() {
+    maxScroll = $navEl.length * $navEl.height() - $(window).height();
+    $sidebar.css("transform", "translateY(0)");
+  });
+  
 });
